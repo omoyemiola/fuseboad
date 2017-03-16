32991c814735e20dd0b5b3e08b8d22a5a453af05
Omoyemi Ajanaku <omoyemiola@gmail.com>
2017-03-16 13:20:18 +0100

    Truly Born for a reason. That I know

---
+++ born.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "I was born for a reason. ";
+
+echo "I am born great and because I know who I am , I cannot be ashamed!";
+
+?>
---
+++ collaborating.php
@@ -0,0 +1,9 @@
+<?php
+
+	echo "The name of Jesus is Higher than all other names.";
+
+	echo "I am not ashamed to work in the vineyard of the Lord.";
+
+	echo "The Lord has prospered me. How can I be ashamed. No , Not at all!!!";
+
+?>
