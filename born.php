48af00c6674e4f6c67710729339579a396e61647
Omoyemi Ajanaku <omoyemiola@gmail.com>
2017-03-16 13:28:22 +0100

    Login Page for the Titans's Admin

--- born.php
+++ born.php
@@ -5,3 +5,20 @@ echo "I was born for a reason. ";
 echo "I am born great and because I know who I am , I cannot be ashamed!";
 
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Training</title>
+</head>
+<body>
+
+<form>
+<p>Admin Name : <input type="text" name="admin_name"></p>
+<p>Password : <input type="password" name="password"></p>
+<p>Type Password Again : <input type="password" name="secured_password"></p>
+
+</form>
+</body>
+</html>
