7d48253a8c7cad13ccc8b04b22329290abb29d1c
Omoyemi Ajanaku <omoyemiola@gmail.com>
2017-03-17 11:52:41 +0100

    From Mr Ibrahim new Update.

---
+++ www/Login.php
@@ -0,0 +1,44 @@
+<html>
+
+<head>
+    <title>Login System</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+
+    <div class="nav">
+
+        <div class="logo">Login System</div>
+
+    </div>
+    <div class="navbar">
+
+        <div class="option">
+            <a href="#" class="option-link">Log In</a>
+        </div>
+        <div class="option">
+            <a href="#" class="option-link">Sign up</a>
+        </div>
+        <div style="clear: both;"></div>
+
+    </div>
+    <div class="container">
+
+        <form>
+            Email <br />
+            <input type="text" class="login-input" name="email"/> <br />
+
+            Password
+            <input type="password" class="login-input" name="password"/> <br />
+
+            <a href="#" class="forgot-password">Forgot password?</a>
+
+            <input type="submit" value="Log In!" id="login-button"/>
+
+        </form>
+
+    </div>
+</body>
+
+</html>
