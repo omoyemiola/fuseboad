e808e996af17c3609558755fb19cf6a6a8b7fc17
Omoyemi Ajanaku <omoyemiola@gmail.com>
2017-03-21 10:17:14 +0100

    as requested by Mr Taiwo

---
+++ shopping.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome Here</title>
+</head>
+<body>
+
+<form action="" method="POST">
+	
+	<p>Firstname: <input type="text" name="fname"></p>
+
+
+</form>
+
+</body>
+</html>
